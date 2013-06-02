db9fc498a08e957104e55d40f6dcb7348a7400b7
David Nuon <david@davidnuon.com>
2013-06-01 23:48:57 -0700

    Restructured

---
+++ content.php
@@ -0,0 +1,36 @@
+<?php include_once("inc/constants.php"); ?>
+<?php include_once("inc/functions.php"); ?>
+<?php include("inc/header.php"); ?>
+
+
+<div class="content-wrapper" style="height:100px">
+	<div class="container_12">
+		<h1>Test for All</h1>
+	</div>
+</div>
+
+
+<div class="container_12">
+	<div class="grid_12">
+		<h2>This is a test</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, atque deleniti in maxime iusto vel asperiores voluptatibus dolore. Hic, eligendi voluptatem perferendis unde corporis ullam iusto omnis quo harum quisquam.</p>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, cupiditate tempora saepe repellat consequatur distinctio est error laudantium dolores minima asperiores odit porro dicta accusantium quas similique tempore modi quisquam.</p>
+
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, ipsam, minima, commodi amet velit necessitatibus officiis earum explicabo consequatur natus iure excepturi quas harum delectus id dignissimos cupiditate placeat quasi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, reiciendis doloremque temporibus iste omnis minima consequatur? Dolorum, porro blanditiis ipsum facere accusantium explicabo dolor nulla tempore magni sint repudiandae voluptatem? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, laboriosam, optio, animi expedita est excepturi velit corporis qui eos cum ad quidem repudiandae tempore accusantium unde earum nulla natus possimus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, voluptates, blanditiis, fugiat mollitia minima tempore alias eaque provident iste nihil adipisci iusto sunt perspiciatis quo officiis placeat ipsam consequatur amet.</p>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque sed sunt placeat dolore totam atque incidunt tenetur. Amet, vero, magni, aliquam, reiciendis quasi veniam labore numquam nulla molestiae ex omnis.</p>
+
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, quibusdam, dolores, architecto recusandae nam reprehenderit vitae eum reiciendis dolor quasi quae magni error eveniet deserunt maxime autem nihil dicta nesciunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, dignissimos qui iusto error commodi dolor cupiditate est sunt ipsam illo non impedit alias velit animi corporis sed hic fuga corrupti!</p>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, eligendi, doloremque, quia, odio est veritatis sunt quaerat deserunt repellat alias animi asperiores. Expedita amet quidem inventore illo quam incidunt soluta.</p>
+	</div>
+</div>
+
+<div class="container_12">
+	<div class="grid_12">
+		<hr>
+	</div>
+</div>
+
+<?php include("inc/footer.php"); ?>
