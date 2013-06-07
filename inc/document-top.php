03dc5c5f126189c078cc773a9cafe8b76c821c91
David Nuon <david@davidnuon.com>
2013-06-07 10:13:24 -0700

    Added comments to cSS

--- inc/document-top.php
+++ inc/document-top.php
@@ -5,8 +5,10 @@
 <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic|Open+Sans:300italic,400,300,600' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="css/960.css">
 
+		<link rel="stylesheet" href="css/structure.css">
 		<link rel="stylesheet" href="css/skin.css">
-		<link rel="stylesheet" href="css/home.css">
+	
 		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/home.css">
 	</head>
 	<body>
--- inc/header.php
+++ inc/header.php
@@ -4,13 +4,13 @@
 	<div class="container_12">
 	
 		<div class="grid_4">
-			<div id="logo">
+			<div class="logo">
 				<img src="img/logo.png" alt="">
 			</div>		
 		</div>
 
 		<div class="grid_8 push_1">
-			<nav>
+			<nav class="main-overhead-navi horizontal-navi">
 				<ul>
 					<li><a href="#">Projects</a></li>
 					<li><a href="#">Events</a></li>
