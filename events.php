8e2d36b734985198c4007c5c9a9c5e146afa8ebd
David Nuon <david@davidnuon.com>
2013-06-08 16:23:57 -0700

    Added effects to event list page

--- events.php
+++ events.php
@@ -2,16 +2,21 @@
 <?php include_once("inc/functions.php"); ?>
 <?php include("inc/header.php"); ?>
 
-<div class="over-container" style="padding:100px 0 0 0; min-height:800px">
+<div class="event-poster-container" style="padding:100px 0 0 0; min-height:800px">
+	<div class="container_12">
+		<div class="grid_12">
+			<h2>Fall 2013</h2>
+		</div>
+	</div>
 	<?php include("inc/events-chart.php"); ?>
 	<?php include("inc/events-chart.php"); ?>
 	<?php include("inc/events-chart.php"); ?>
 </div>
 
-<script>
-	(function () {
-		console.log(document.body.setAttribute('class', 'events-body'));
-	})();
-</script>
 
+<script>
+	window.onload = function () {
+		document.body.setAttribute('class', 'events-list-body')
+	}
+</script>
 <?php include("inc/footer.php"); ?>
--- inc/events-chart.php
+++ inc/events-chart.php
@@ -1,7 +1,11 @@
 <div class="container_12">
-	<a href="#" class="grid_3 event-item"></a>
-	<a href="#" class="grid_3 event-item"></a>
-	<a href="#" class="grid_3 event-item"></a>
-	<a href="#" class="grid_3 event-item"></a>
+	<?php for ($x=0; $x < 4; $x++): ?>
+	<a href="#" class="grid_3 event-item">
+		<img src="img/posters/<?php echo rand(1, 6); ?>.png" alt="">
+		<span class="event-info">
+
+		</span>
+	</a>
+	<?php endfor; ?>	
 </div>
 <div class="container_12"><br></div>
