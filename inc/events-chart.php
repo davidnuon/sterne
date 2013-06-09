5117845b37f4742e1496366d6efe36a65b679762
David Nuon <david@davidnuon.com>
2013-06-08 18:27:20 -0700

    Added filler text to event page

--- inc/events-chart.php
+++ inc/events-chart.php
@@ -2,9 +2,12 @@
 	<?php for ($x=0; $x < 4; $x++): ?>
 	<a href="#" class="grid_3 event-item">
 		<img src="img/posters/<?php echo rand(1, 6); ?>.png" alt="">
-		<span class="event-info">
-			<span class="title">
-				Flat is the new Black
+		<span class="event-info vevent"  id="hcalendar-Flat-is-the-New-Black">
+			<span class="summary title">Flat is the New Black</span>
+			<time datetime="2013-06-08" class="data-item time dtstart">8 June 2013</time>
+			<span class="location data-item">ECS-304</span>
+			<span class="description data-item">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 			</span>
 		</span>
 	</a>
